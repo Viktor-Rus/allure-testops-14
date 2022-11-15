36569fc2f89fbf2931860413cbd7810b8dfc3fbe
Viktor_Rus <vaurusov@mts.ru>
2022-11-15 15:00:23 +0300

    added second test

--- src/test/java/allure/TestExample.java
+++ src/test/java/allure/TestExample.java
@@ -1,5 +1,6 @@
 package allure;
 
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,14 +9,25 @@ import static io.qameta.allure.Allure.step;
 
 public class TestExample {
 
-    @Test()
-    @Owner("vaurusov")
-    @DisplayName("Проверка отображения логотипа на главной странице")
-    void mainLogoVisible() {
-        step("Проверка отображаения логотипа на главной странице ", ()-> {
-            System.out.println("Удачно");
-        });
 
+    @Test()
+    @AllureId("12898")
+    @DisplayName("Авторизация через google")
+    @Owner("allure8")
+    void authorizationWithGoogle() {
+        step("Открываем главную страницу");
+        step("Кликаем на значок авторизации через Google");
+        step("Вводим логин/пароль и нажимаем кнопку Войти");
+        step("Проверяем что пользователь авторизовался");
+    }
+
+    @Test
+    @AllureId("12905")
+    @DisplayName("Проверка отображения логотипа на главной странице")
+    @Owner("allure8")
+    void checkLogoVisible() {
+        step("Перейти на главную страницу приложения");
+        step("Убедиться что в левом верхнем углу отображается логотип приложения");
     }
 
 
